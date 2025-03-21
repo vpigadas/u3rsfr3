c3f6cb4b96dc8f2e8e5f114288982c094769f35c
Pigadas <vasssilis.pigadas@printecgroup.com>
2025-03-21 17:55:49 +0200

    initialise

---
+++ app/src/main/java/eu/york/u3rsfr3/LoginActivity.java
@@ -0,0 +1,18 @@
+package eu.york.u3rsfr3;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.login_button);
+        button.setText("Login Button");
+    }
+}
---
+++ app/src/main/java/eu/york/u3rsfr3/networking/NetworkActivity.java
@@ -0,0 +1,54 @@
+package eu.york.u3rsfr3.networking;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import eu.york.u3rsfr3.R;
+
+public class NetworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network);
+
+
+        sentRequest();
+    }
+
+    private void sentRequest() {
+        TextView textView = findViewById(R.id.network_title);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://dog.ceo/api/breeds/image/random";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+}
